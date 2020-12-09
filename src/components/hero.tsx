4ef411362a0a5b7821c4223ac858931ec969de5c
alexliusq <uilxela7@gmail.com>
2020-12-09 21:07:42 +0800

    basically got your minimalist landing page figured out

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -4,6 +4,7 @@ import Divider from "./divider-parallax"
 // import Inner from "../elements/inner"
 import Content from "./content-parallax"
 import SVG from "./svg"
+import { ParallaxLayer } from "react-spring/renderprops-addons"
 // import { UpDown, UpDownWide } from "../styles/animations"
 // @ts-ignore
 // import Intro from "../sections/intro"
@@ -15,47 +16,12 @@ interface HeroProps {
 
 const Hero: React.FC<HeroProps> = ({ offset, factor = 1, children }) => (
   <div>
-    <Divider speed={0.2} offset={offset} factor={factor}>
-      <div sx={{
-        position: 'absolute',
-        left: '45%',
-        top: '17%',
-        opacity: '50%',
-        fontSize: '3em'
-      }}>
-        ✍
-      </div>
-      <div sx={{
-        position: 'absolute',
-        left: '75%',
-        top: '18%',
-        opacity: '50%',
-        fontSize: '5em'
-      }}>
-        👨‍💻
-      </div>
-      <div
-        sx={{
-          position: 'absolute',
-          left: '15%',
-          top: '20%',
-          opacity: '50%',
-          fontSize: '4em'
-        }}
-      >
-        📚
-      </div>
-      <div
-        sx={{
-          position: 'absolute',
-          left: '10%',
-          top: '70%',
-          opacity: '50%',
-          fontSize: '3em'
-        }}
-      >
-        🏋️
-      </div>
+    {/* <Divider speed={2} offset={offset} factor={factor}> */}
+    <ParallaxLayer
+      offset={offset}
+      speed={4}
+      factor={factor}
+    >
       <div
         sx={{
           position: 'absolute',
@@ -78,8 +44,62 @@ const Hero: React.FC<HeroProps> = ({ offset, factor = 1, children }) => (
       >
         🎵🕺
       </div>
-    </Divider>
-    <Content sx={{ variant: `texts.bigger` }} speed={0.6} offset={offset} factor={factor}>
+    </ParallaxLayer>
+    <ParallaxLayer
+      offset={offset}
+      speed={3}
+      factor={factor}
+    >
+      <div sx={{
+        position: 'absolute',
+        left: '45%',
+        top: '17%',
+        opacity: '50%',
+        fontSize: '3em'
+      }}>
+        ✍
+      </div>
+      <div
+        sx={{
+          position: 'absolute',
+          left: '10%',
+          top: '70%',
+          opacity: '50%',
+          fontSize: '3em'
+        }}
+      >
+        🏋️
+      </div>
+    </ParallaxLayer>
+    <ParallaxLayer
+      offset={offset}
+      speed={2}
+      factor={factor}
+    >
+      <div sx={{
+          position: 'absolute',
+          left: '75%',
+          top: '18%',
+          opacity: '50%',
+          fontSize: '5em'
+      }}>
+          👨‍💻
+      </div>
+      <div
+        sx={{
+          position: 'absolute',
+          left: '15%',
+          top: '20%',
+          opacity: '50%',
+          fontSize: '4em'
+        }}
+      >
+      📚
+      </div>
+    </ParallaxLayer>
+      
+    {/* </Divider> */}
+    <Content sx={{ variant: `texts.bigger` }} speed={1} offset={offset} factor={factor}>
       {children}
     </Content>
   </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,13 @@
 import React, { Fragment } from 'react';
 import { jsx } from 'theme-ui';
 import Hero from '../components/hero';
-import { Parallax } from 'react-spring/renderprops-addons';
+import { Parallax, ParallaxLayer } from 'react-spring/renderprops-addons';
 import { Link } from 'gatsby';
+import Content from '../components/content-parallax';
 
 export default function Home() {
   return (
-    <Parallax pages={2}>
+    <Parallax pages={1.25}>
       <Hero offset={0} factor={1}>
         <h1>
           Hi, I'm Alex Liu
@@ -20,9 +21,26 @@ export default function Home() {
           I <Link to='/blog/why-i-quit-trading'>quit my job as a trader</Link>, built an app during COVID, and now I'm looking for a job as a software engineer
         </p>
         <p>
-          I read, write, code, lift, and invest.
+          I read, write, code, lift, invest, and dance.
         </p>
       </Hero>
+      <Content
+        speed={1}
+        offset={0.75}
+        factor={0.5}
+      >
+        <h1>
+          More
+        </h1>
+        <ul sx={{paddingInlineStart: 0}}>
+          <li>
+            <Link to='/blog'>Blog</Link>
+          </li>
+          <li>
+            <Link to='/about'>About</Link>
+          </li>
+        </ul>
+      </Content>
   </Parallax>
   )
 }
