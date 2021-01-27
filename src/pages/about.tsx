2da1516d12e8848eb3bb49c8141485f25e86b1be
alexliusq <uilxela7@gmail.com>
2021-01-27 23:37:22 +0800

    updated about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -19,24 +19,47 @@ const StyledLink: React.FC<{to: string}>= ({ to, children }) => {
 
 export default function About() {
   return (
-    <div
-      sx={{
-        display: 'flex',
-        justifyContent: 'center'
-      }}
-    >
-      <div
-        sx={{
-          marginX: '40px',
-          paddingTop: '40px',
-          maxWidth: '800px'
-        }}
+    // <div
+    //   sx={{
+    //     display: 'flex',
+    //     justifyContent: 'center'
+    //   }}
+    // >
+    //   <div
+    //     sx={{
+    //       marginX: '40px',
+    //       paddingTop: '40px',
+    //       maxWidth: '800px'
+    //     }}
+    //   >
+    <Parallax pages={1.5}>
+      <Hero offset={0} factor={0.7}>
+        <Styled.h1>
+          Hi, I'm Alex Liu
+        </Styled.h1>
+        {/* <Styled.h6
+          sx={{
+            textAlign: 'center',
+            paddingX: '20px',
+            maxWidth: '800px'
+          }}
+        >
+          I <StyledLink to='/blog/why-i-quit-trading'>quit my job</StyledLink> as a trader, <StyledLink to='/portfolio/margins/overview'>built an app</StyledLink> during COVID,
+          and now I'm looking for a job as a software engineer. Here's <StyledLink to='/blog/why-code-why-me'>why I might be a fit</StyledLink> for your team.
+        </Styled.h6> */}
+        <Styled.p>
+          I read, write, code, lift, invest, and dance.
+        </Styled.p>
+      </Hero>
+      <Content
+        speed={1}
+        offset={0.75}
+        factor={1}
       >
-        <Styled.h1>About</Styled.h1>
-      
+        <Styled.h2>A Brief History</Styled.h2>
         <Styled.p>
         My parents each brought 2 suitcases, and 2 years later, me, from China to Silicon Valley,
-        where I grew up in an Asian Tech Immigrant's version of surburbia. English became my native tongue
+        where I had my childhood in an Asian Tech Immigrant's version of surburbia. English became my native tongue
         and the local public library became my second home.
         </Styled.p>
         <Styled.p>
@@ -44,15 +67,15 @@ export default function About() {
         a city at the forefront of a country convulsing through 300 years of economic transformation in 30.
         Looking back, moving to China was one of the best changes my family made. 
         I attended the international division of a local high school,
-        which combined an expansive Western curriculum with Chinese relentlessness
-        and cash incentives for high standardized test scores, students and teachers included.
+        which combined an expansive Western curriculum with Chinese discipline
+        and cash incentives for high standardized test scores which were given to both students and teachers.
         </Styled.p>
         <Styled.p>
         For college, I traded smelling smog to smelling cows in pastoral New England, where I both enjoyed the luxury of a liberal arts
         education and chafed at some of its anachronisms. I regret not transferring to a larger research university, but I'm forever grateful for the
         friends, professors, and staff members I met at Amherst College. It's a special place with an especially dedicated alumni fundraising effort.
         </Styled.p>
-        <Styled.p>
+        {/* <Styled.p>
         The benefit and brutality of an increasingly globalized world have been the most definitive forces shaping my life. I am enormously lucky to have grown up as an 
         unwitting beneficiary of globalization. Now though, I have the full responsibility to make the most of my situation. As one of my favorite writers,
         Mohsin Hamid, notes: 
@@ -60,8 +83,12 @@ export default function About() {
         “But if globalization is capable of holding out any fundamental promise to us, any temptation to go along with its havoc, then surely that promise ought to be this: we will be more free to invent ourselves.”
         </Styled.blockquote>
         If anything, quitting my job before a global pandemic is an act of self-invention. 
-        </Styled.p>
-      </div>
-    </div>
+        </Styled.p> */}
+      </Content>
+  </Parallax>
+
+
+    //   </div>
+    // </div>
   )
 }
